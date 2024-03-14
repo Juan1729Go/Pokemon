8581aaac94034539667d9bc23b38299e4505ed16
Juan Sebastian Gonzalez Rojas <96216286+Juan1729Go@users.noreply.github.com>
2024-03-14 15:01:48 -0500

    Delete services directory

--- services/verify.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Verificar si el usuario no está logueado
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: ../index.php"); // Redirigir a la página de login si no está logueado
-    exit();
-}
-?>
