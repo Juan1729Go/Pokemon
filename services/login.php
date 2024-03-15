18dc9240170c448023e7e58fca5e0fd46d03c68b
Juan Sebastian Gonzalez Rojas <96216286+Juan1729Go@users.noreply.github.com>
2024-03-15 07:04:12 -0500

    Update login.php

--- services/login.php
+++ services/login.php
@@ -5,7 +5,6 @@ require_once '../services/db.php';
 
 $error = ""; 
 
-// Verify if the form has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
