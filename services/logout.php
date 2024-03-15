64aaf101a5c6e871718c57576f65ecff141c7597
Juan Sebastian Gonzalez Rojas <96216286+Juan1729Go@users.noreply.github.com>
2024-03-15 07:02:43 -0500

    Update logout.php

--- services/logout.php
+++ services/logout.php
@@ -1,19 +1,18 @@
 <?php
-// Iniciar la sesión si no está iniciada
 session_start();
 
 // Destruir todas las variables de sesión
 $_SESSION = array();
 
-// Borrar la cookie de sesión
+// Borrar cookie
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time()-42000, '/');
 }
 
-// Finalmente, destruir la sesión
+//destruir la sesión
 session_destroy();
 
-// Redirigir a la página de inicio de sesión u otra página apropiada
+// Redirigir
 header("Location: ../index.php");
 exit;
 ?>
